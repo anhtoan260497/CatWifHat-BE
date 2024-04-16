a3de1aad992d908d985971c63123d7f46f300292
anhtoan260497 <anhtoan260497@gmail.com>
2024-04-16 15:38:31 +0700

    initial BE

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.7",
+      },
+      // {
+      //   version: "0.6.7",
+      //   settings: {},
+      // },
+    ],
+  },
+};
